74991d234b3026db6a2f52701cfba4aee983f68a
Daniel Quevedo <danielelvisquevedo@gmail.com>
2025-04-07 21:05:32 -0300

    feat: UserName component example

---
+++ src/UserName.tsx
@@ -0,0 +1,20 @@
+const UserName = () => {
+    return (
+        <h4>My name is Daniel!</h4>
+    )
+}
+
+const UserLastName = () => {
+    return (
+        <h4>My last name is Quevedo!</h4>
+    )
+}
+
+const UserAge = () => {
+    return (
+        <h4>Myage is 27!</h4>
+    )
+}
+
+export default UserAge;
+export { UserName, UserLastName };
